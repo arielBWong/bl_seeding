2cb9b5f764fd2134b7ee45f3008f944139658260
arielBWong <ariel.bingwang@outlook.com>
2022-03-25 14:42:37 +1100

    fix FE

---
+++ loadparameter.m
@@ -0,0 +1,20 @@
+
+predefined_FE_allproblems = struct();
+
+predefined_FE_allproblems.SMD1mp = 60000;
+predefined_FE_allproblems.SMD2mp = 60000;
+predefined_FE_allproblems.SMD3mp = 60000;
+predefined_FE_allproblems.SMD4mp = 60000;
+predefined_FE_allproblems.SMD5mp = 60000;
+predefined_FE_allproblems.SMD6mp = 60000;
+predefined_FE_allproblems.SMD7mp = 60000;
+predefined_FE_allproblems.SMD8mp = 60000;
+
+predefined_FE_allproblems.SMD1 =  40000;
+predefined_FE_allproblems.SMD2 =  40000;
+predefined_FE_allproblems.SMD3 =  60000;
+predefined_FE_allproblems.SMD4 =  50000;
+predefined_FE_allproblems.SMD5 =  50000;
+predefined_FE_allproblems.SMD6 =  50000;
+predefined_FE_allproblems.SMD7 =  40000;
+predefined_FE_allproblems.SMD8 =  60000;
